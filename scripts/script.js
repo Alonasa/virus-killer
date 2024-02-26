2b2becbf4a66e3cbfbb3de1c08b21b88a64fa02e
Alonesa <alenakrasnov@mail.ru>
2024-02-26 12:33:36 +0000

    improve game, added time counter to fix records, rename variables

--- scripts/script.js
+++ scripts/script.js
@@ -1,79 +1,100 @@
 let size = 10;
+let score = 0;
 
-    function randomInteger(min, max) {
-        // получить случайное число от (min-0.5) до (max+0.5)
-        let rand = min - 0.5 + Math.random() * (max - min + 1);
-        return Math.round(rand);
+let lastRecord = 1000;
+let newRecord = 0;
+
+const rows = document.getElementsByTagName("tr");
+const cells = document.getElementsByTagName("td");
+
+let virusX = randomInteger(1, rows.length - 1);
+let virusY = randomInteger(1, rows.length - 1);
+
+let doctorX = 0;
+let doctorY = 0;
+
+const virus = document.createElement("img");
+virus.src = "img/virus.png";
+const doctor = document.createElement("img");
+doctor.src = "img/doctor1.png";
+
+function countRecord() {
+  newRecord += 1;
+  console.log(`You got: ${newRecord}`)
+}
+
+function randomInteger(min, max) {
+  // Get random amount (min-0.5) до (max+0.5)
+  let rand = min - 0.5 + Math.random() * (max - min + 1);
+  return Math.round(rand);
+}
+
+setInterval(countRecord, 1000)
+
+function moveDoctor() {
+  const counter = document.getElementsByClassName("counter-amount");
+  const audios = document.getElementsByTagName("audio");
+  const doctorIndex = doctorX + doctorY * rows.length;
+  
+  
+  if (doctorX === virusX && doctorY === virusY) {
+    while (doctorX === virusX && doctorY === virusY) {
+      virusX = randomInteger(0, rows.length - 1);
+      virusY = randomInteger(0, rows.length - 1);
     }
-
-    var virus = document.createElement("img");
-    virus.src = "img/virus.png";
-    var doctor = document.createElement("img");
-    doctor.src = "img/doctor1.png";
-
-    var rows = document.getElementsByTagName("tr");
-    var cells = document.getElementsByTagName("td");
-
-    var doctorX = 0;
-    var doctorY = 0;
-
-    function moveDoctor() {
-
-        if (doctorX === virusX && doctorY === virusY) {
-            while (doctorX === virusX && doctorY === virusY){
-                virusX = randomInteger(0, rows.length - 1);
-            virusY = randomInteger(0, rows.length - 1);
-            }
-            
-            var spans = document.getElementsByTagName("span");
-            spans[0].innerText++;
-            var audios = document.getElementsByTagName("audio");
-            audios[0].currentTime = 0;
-            audios[0].play();
-            moveVirus();
-        }
-
-        var doctorIndex = doctorX + doctorY * rows.length;
-        cells[doctorIndex].append(doctor);
-    }
-
-    moveDoctor();
-
-    var virusX = randomInteger(1, rows.length -1);
-    var virusY =randomInteger(1, rows.length -1);
     
-    function moveVirus() {
-        var virusIndex = virusX + virusY * rows.length;
-        cells[virusIndex].append(virus);
-    }
+    score += 1;
+    counter[0].innerText++;
+    audios[0].currentTime = 0;
+    audios[0].play();
     moveVirus();
     
-    document.onkeypress = function(event) {
-        if (event.code === "KeyW") {
-            if (doctorY > 0){
-                doctorY--;
-            }
-        }
-
-        if (event.code === "KeyS") {
-            if (doctorY < rows.length - 1) {
-                doctorY++;
-            }
-        }
-
-        if (event.code === "KeyD") {
-            if (doctorX < rows.length -1) {
-                doctorX++;
-            }
-        }
-        
-        if (event.code === "KeyA") {
-            if (doctorX > 0) {
-                doctorX--;
-            }
-        }
-
-        moveDoctor();
-        console.log("key pressed: " + event.code);
+    if (score % size === 0) {
+      let amtTime = newRecord > 60 ? newRecord / 60 : newRecord
+      if (newRecord < lastRecord){
+        lastRecord = newRecord;
+        alert(`You hit ${size} viruses about ${amtTime.toFixed(0)} seconds`)
+      }
+      newRecord = 0;
     }
-    
+  }
+  console.log(score)
+  
+  cells[doctorIndex].append(doctor);
+}
+
+function moveVirus() {
+  let virusIndex = virusX + virusY * rows.length;
+  cells[virusIndex].append(virus);
+}
+
+document.onkeypress = function (event) {
+  if (event.code === "KeyW") {
+    if (doctorY > 0) {
+      doctorY--;
+    }
+  }
+  
+  if (event.code === "KeyS") {
+    if (doctorY < rows.length - 1) {
+      doctorY++;
+    }
+  }
+  
+  if (event.code === "KeyD") {
+    if (doctorX < rows.length - 1) {
+      doctorX++;
+    }
+  }
+  
+  if (event.code === "KeyA") {
+    if (doctorX > 0) {
+      doctorX--;
+    }
+  }
+  
+  moveDoctor();
+}
+
+moveDoctor();
+moveVirus();
